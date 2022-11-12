5b57a4e7f6676c21a19b2fb5eb374d31970d9f75
Steelking1 <117577502+Steelking1@users.noreply.github.com>
2022-11-12 11:35:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -173,7 +173,6 @@ function carousel() {
             responsive: {
                 0: {
                 items: 1,
-                nav: true
                 },
                 750: {
                 items: 2,
@@ -202,3 +201,20 @@ function carousel() {
     })
 }
 carousel();
+
+function LoginModals() {
+    var btnLogModals = document.querySelectorAll('.toggle-modal')
+    var Modals = document.querySelector('.modals-container')
+    btnLogModals.forEach(btn => btn.addEventListener('click', () => {
+        Modals.classList.toggle('active')
+        window.addEventListener('scroll', ()=>{
+            if (Modals.className === 'modals-container active') {
+                window.scrollTo(0,0);
+            }
+        });
+        const audio = new Audio();
+        audio.src = 'audio/zapsplat_multimedia_button_click_004_78081.mp3'
+        audio.play();
+    }))
+}
+LoginModals();
